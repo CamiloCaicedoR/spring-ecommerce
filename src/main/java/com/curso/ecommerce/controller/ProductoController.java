f00061f13d06b2c76feeb6af722fe0188250d1f0
Camilo Caicedo <camilocaicedo353@gmail.com>
2026-04-26 14:02:52 -0500

    se crea el repository y las clases de servicio y se agrega metodo show de ProductController

---
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -0,0 +1,15 @@
+package com.curso.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+
+    @GetMapping("")
+    public String show() {
+        return "productos/show";
+    }
+}
