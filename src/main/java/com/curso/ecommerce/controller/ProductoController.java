ec8e04f7d7e0f1718c0abe5089bfba37e0c1b4d8
Camilo Caicedo <camilocaicedo353@gmail.com>
2026-05-01 12:43:53 -0500

    Se agrega funcionalidad para editar y eliminar registros

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controller;
 
+import java.lang.foreign.Linker.Option;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.model.Usuario;
@@ -41,4 +44,28 @@ public class ProductoController {
         productoService.save(producto);
         return "redirect:/productos";
     }   
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+        
+        LOGGER.info("Producto a editar: " + producto);
+        model.addAttribute("producto", producto);
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto) {
+        LOGGER.info("Producto a actualizar: " + producto);
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        productoService.delete(id);
+        return "redirect:/productos";
+    }
 }
